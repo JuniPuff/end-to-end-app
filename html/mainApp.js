27822938f0939a476f05232c39179396c89b2940
JuniPuff <juniper@todandlorna.com>
2019-04-05 16:48:04 -0600

    removed code where I was using said unnecessary data

--- html/mainApp.js
+++ html/mainApp.js
@@ -2,7 +2,6 @@ var app = angular.module('mainApp', []);
 app.controller('mainCtrl', function(){
     var me = this;
     me.projectPosts = projectPosts;
-    me.udemyPosts = udemyPosts;
     me.displayPopUpToggle = true;
     me.displayPopUp = function(){
         me.displayPopUpToggle = !me.displayPopUpToggle
