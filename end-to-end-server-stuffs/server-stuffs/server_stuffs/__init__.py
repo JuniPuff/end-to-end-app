20ed7f2f0ee0eae93dd47f82721f123cb907cd33
JuniPuff <juniper@mothwithalamp.com>
2019-04-18 12:09:16 -0600

    Start with gunicorn

--- end-to-end-server-stuffs/server-stuffs/server_stuffs/__init__.py
+++ end-to-end-server-stuffs/server-stuffs/server_stuffs/__init__.py
@@ -5,8 +5,9 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    config.include('webtest')
+    config.include('gunicorn')
     config.include('pyramid_jinja2')
     config.include('.models')
-    config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
