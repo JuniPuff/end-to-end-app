b5c194943388b3fee41b80ff919705537016cf1e
JuniPuff <juniper@todandlorna.com>
2019-06-24 14:32:31 -0600

    Fix typo

--- end-to-end-server-stuffs/server-stuffs/test-gunicorn.py
+++ end-to-end-server-stuffs/server-stuffs/test-gunicorn.py
@@ -2,7 +2,7 @@ def gunicornstuffs(environ, start_response):
     """Simplest possible application object"""
     data = 'Hello, World!\n'
     status = '200 OK'
-    resopnse_headers = [
+    response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(data)))
     ]
