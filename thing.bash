2eb1afea9b90b975de8d9b58bdc04a2f7a42416b
JuniPuff <juniper@todandlorna.com>
2019-06-24 15:54:47 -0600

    Update bash file

--- thing.bash
+++ thing.bash
@@ -3,4 +3,8 @@
 ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ubuntu@3.18.103.85 '
 cd /var/www/
 git pull
+cd end-to-end-server-stuffs/env/bins/
+source activate
+cd ../../server-stuffs
+gunicorn --bind=localhost:8000 --workers=2 test-gunicorn:gunicornstuffs
 '
