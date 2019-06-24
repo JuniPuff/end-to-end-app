0fe245da14fc449b8901dfe80cb5e6fd875973c1
JuniPuff <juniper@mothwithalamp.com>
2019-06-24 15:58:34 -0600

    Fix path to virtualenv scripts

--- thing.bash
+++ thing.bash
@@ -3,7 +3,7 @@
 ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ubuntu@3.18.103.85 '
 cd /var/www/
 git pull
-cd /var/www/end-to-end-server-stuffs/env/bins/
+cd /var/www/end-to-end-server-stuffs/env/bin/
 source activate
 cd /var/www/end-to-end-server-stuffs/server-stuffs
 gunicorn --bind=localhost:8000 --workers=2 test-gunicorn:gunicornstuffs
