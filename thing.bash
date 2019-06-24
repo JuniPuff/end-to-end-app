8e61f949d81a37d840e390bea9de8e1cde578f34
JuniPuff <juniper@todandlorna.com>
2019-06-24 16:52:34 -0600

    Remove start gunicorn commands

--- thing.bash
+++ thing.bash
@@ -3,10 +3,4 @@
 ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ubuntu@3.18.103.85 '
 cd /var/www/
 git pull
-cd /var/www/end-to-end-server-stuffs/env/bin/
-source activate
-echo "Activate virtualenv"
-cd /var/www/end-to-end-server-stuffs/server-stuffs
-gunicorn --bind=localhost:8000 --workers=2 test-gunicorn:gunicornstuffs
-echo "Activate gunicorn stuffs"
 '
