a860987ee6ce2c65e8768862b635d0ef98cf5b8d
JuniPuff <juniper@todandlorna.com>
2019-06-24 16:05:13 -0600

    Add echo commands to bash file

--- thing.bash
+++ thing.bash
@@ -5,6 +5,8 @@ cd /var/www/
 git pull
 cd /var/www/end-to-end-server-stuffs/env/bin/
 source activate
+echo "Activate virtualenv"
 cd /var/www/end-to-end-server-stuffs/server-stuffs
 gunicorn --bind=localhost:8000 --workers=2 test-gunicorn:gunicornstuffs
+echo "Activate gunicorn"
 '
