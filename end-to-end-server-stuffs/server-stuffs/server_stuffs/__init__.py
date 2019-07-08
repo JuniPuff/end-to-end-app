5de50e436dd9e3e1623adf4c5e2de3ebf9f8e345
JuniPuff <juniper@mothwithalamp.com>
2019-07-08 15:39:50 -0600

    change configurations and stuff

--- end-to-end-server-stuffs/server-stuffs/server_stuffs/__init__.py
+++ end-to-end-server-stuffs/server-stuffs/server_stuffs/__init__.py
@@ -9,5 +9,6 @@ def main(global_config, **settings):
     config.include('gunicorn')
     config.include('pyramid_jinja2')
     config.include('.models')
+    config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
