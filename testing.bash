29a0f7699dc9e1904af79ad53636ab6d9e868817
JuniPuff <juniper@todandlorna.com>
2019-08-28 12:51:35 -0600

    Run each test file individually for now

--- testing.bash
+++ testing.bash
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-python3 -m pytest end-to-end-server-stuffs/server-stuffs/server_stuffs/tests
+python3 -m pytest end-to-end-server-stuffs/server-stuffs/server_stuffs/tests/session_tests.py
+python3 -m pytest end-to-end-server-stuffs/server-stuffs/server_stuffs/tests/task_tests.py
+python3 -m pytest end-to-end-server-stuffs/server-stuffs/server_stuffs/tests/tasklist_tests.py
+python3 -m pytest end-to-end-server-stuffs/server-stuffs/server_stuffs/tests/user_tests.py
