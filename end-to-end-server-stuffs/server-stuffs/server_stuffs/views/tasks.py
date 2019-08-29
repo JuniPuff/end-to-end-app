602f7a15219a9a074a1af1d5ed27ee563799fa24
JuniPuff <juniper@mothwithalamp.com>
2019-08-29 09:33:30 -0600

    Change status_code to code for httpexceptions

--- end-to-end-server-stuffs/server-stuffs/server_stuffs/views/tasks.py
+++ end-to-end-server-stuffs/server-stuffs/server_stuffs/views/tasks.py
@@ -13,24 +13,24 @@ from ..scripts.utilities import error_dict
 def tasks(request):
     if request.method == 'GET':
         if request.user is None:
-            status_code = httpexceptions.HTTPUnauthorized.status_code
+            status_code = httpexceptions.HTTPUnauthorized.code
             result = error_dict("api_error", "not authenticated for this request")
         elif request.GET.get("list_id") is None:
-            status_code = httpexceptions.HTTPBadRequest.status_code
+            status_code = httpexceptions.HTTPBadRequest.code
             result = error_dict("api_error", "list_id is required")
         else:
             tasklist = request.dbsession.query(TaskListModel)\
                 .filter(TaskListModel.list_id == request.GET.get("list_id")).one_or_none()
             if tasklist is None:
-                status_code = httpexceptions.HTTPNotFound.status_code
+                status_code = httpexceptions.HTTPNotFound.code
                 result = error_dict("api_error", "list doesnt exist")
             elif tasklist.user_id != request.user.user_id:
-                status_code = httpexceptions.HTTPUnauthorized.status_code
+                status_code = httpexceptions.HTTPUnauthorized.code
                 result = error_dict("api_error", "not authenticated for this request")
             else:
                 tasksforlist = request.dbsession.query(TaskModel)\
                     .filter(TaskModel.list_id == request.GET.get("list_id")).all()
-                status_code = httpexceptions.HTTPOk.status_code
+                status_code = httpexceptions.HTTPOk.code
                 result = array_of_dicts_from_array_of_models(tasksforlist)
 
         return Response(
@@ -42,19 +42,19 @@ def tasks(request):
     elif request.method == 'POST':
         body = request.json_body
         if request.user is None:
-            status_code = httpexceptions.HTTPUnauthorized.status_code
+            status_code = httpexceptions.HTTPUnauthorized.code
             result = error_dict("api_error", "not authenticated for this request")
         elif body.get("list_id") is None or body.get("task_name") is None:
-            status_code = httpexceptions.HTTPBadRequest.status_code
+            status_code = httpexceptions.HTTPBadRequest.code
             result = error_dict("api_error", "list_id and task_name are required")
         else:
             tasklist = request.dbsession.query(TaskListModel)\
                 .filter(TaskListModel.list_id == body.get("list_id")).one_or_none()
             if tasklist is None:
-                status_code = httpexceptions.HTTPNotFound.status_code
+                status_code = httpexceptions.HTTPNotFound.code
                 result = error_dict("api_error", "list doesnt exist")
             elif tasklist.user_id != request.user.user_id:
-                status_code = httpexceptions.HTTPUnauthorized.status_code
+                status_code = httpexceptions.HTTPUnauthorized.code
                 result = error_dict("api_error", "not authenticated for this request")
             else:
                 task = TaskModel()
@@ -66,7 +66,7 @@ def tasks(request):
                 # Autocommit is true, but just in case that is turned off, we use refresh, so it pulls the task_id
                 request.dbsession.flush()
                 request.dbsession.refresh(task)
-                status_code = httpexceptions.HTTPOk.status_code
+                status_code = httpexceptions.HTTPOk.code
                 result = dict_from_row(task)
 
         return Response(
@@ -86,25 +86,25 @@ def tasks_by_id(request):
     task_id = request.matchdict.get('task_id')
     if request.method == 'GET':
         if request.user is None:
-            status_code = httpexceptions.HTTPUnauthorized.status_code
+            status_code = httpexceptions.HTTPUnauthorized.code
             result = error_dict("api_error", "not authenticated for this request")
         elif task_id is None:
-            status_code = httpexceptions.HTTPBadRequest.status_code
+            status_code = httpexceptions.HTTPBadRequest.code
             result = error_dict("api_error", "task_id is required")
         else:
             task = request.dbsession.query(TaskModel)\
                 .filter(TaskModel.task_id == task_id).one_or_none()
             if task is None:
-                status_code = httpexceptions.HTTPNotFound.status_code
+                status_code = httpexceptions.HTTPNotFound.code
                 result = error_dict("api_error", "task doesnt exist")
             else:
                 tasklist = request.dbsession.query(TaskListModel)\
                     .filter(TaskListModel.list_id == task.list_id).one_or_none()
                 if tasklist.user_id != request.user.user_id:
-                    status_code = httpexceptions.HTTPUnauthorized.status_code
+                    status_code = httpexceptions.HTTPUnauthorized.code
                     result = error_dict("api_error", "not authenticated for this request")
                 else:
-                    status_code = httpexceptions.HTTPOk.status_code
+                    status_code = httpexceptions.HTTPOk.code
                     result = dict_from_row(task)
 
         return Response(
@@ -116,25 +116,25 @@ def tasks_by_id(request):
     elif request.method == 'PUT':
         body = request.json_body
         if request.user is None:
-            status_code = httpexceptions.HTTPUnauthorized.status_code
+            status_code = httpexceptions.HTTPUnauthorized.code
             result = error_dict("api_error", "not authenticated for this request")
         elif task_id is None or (body.get("task_name") is None and body.get("list_id") is None and body.get("task_done") is None):
-            status_code = httpexceptions.HTTPBadRequest.status_code
+            status_code = httpexceptions.HTTPBadRequest.code
             result = error_dict("api_error", "task_name, list_id, or task_done, and task_id are required")
         elif body.get("task_done") and isinstance(body.get("task_done"), bool) is not True:
-                status_code = httpexceptions.HTTPBadRequest.status_code
+                status_code = httpexceptions.HTTPBadRequest.code
                 result = error_dict("api_error", "task_done must be a boolean")
         else:
             task = request.dbsession.query(TaskModel)\
                 .filter(TaskModel.task_id == task_id).one_or_none()
             if task is None:
-                status_code = httpexceptions.HTTPNotFound.status_code
+                status_code = httpexceptions.HTTPNotFound.code
                 result = error_dict("api_error", "task doesnt exist")
             else:
                 tasklist = request.dbsession.query(TaskListModel)\
                     .filter(TaskListModel.list_id == task.list_id).one_or_none()
                 if tasklist.user_id != request.user.user_id:
-                    status_code = httpexceptions.HTTPUnauthorized.status_code
+                    status_code = httpexceptions.HTTPUnauthorized.code
                     result = error_dict("api_error", "not authenticated for this request")
                 else:
                     if body.get("task_name"):
@@ -145,7 +145,7 @@ def tasks_by_id(request):
                         task.task_done = body.get("task_done")
                     request.dbsession.flush()
                     request.dbsession.refresh(task)
-                    status_code = httpexceptions.HTTPOk.status_code
+                    status_code = httpexceptions.HTTPOk.code
                     result = dict_from_row(task)
 
         return Response(
@@ -156,27 +156,27 @@ def tasks_by_id(request):
         )
     elif request.method == 'DELETE':
         if request.user is None:
-            status_code = httpexceptions.HTTPUnauthorized.status_code
+            status_code = httpexceptions.HTTPUnauthorized.code
             result = error_dict("api_error", "not authenticated for this request")
         elif task_id is None:
-            status_code = httpexceptions.HTTPBadRequest.status_code
+            status_code = httpexceptions.HTTPBadRequest.code
             result = error_dict("api_error", "task_id is required")
         else:
             task = request.dbsession.query(TaskModel)\
                 .filter(TaskModel.task_id == task_id).one_or_none()
             if task is None:
-                status_code = httpexceptions.HTTPNotFound.status_code
+                status_code = httpexceptions.HTTPNotFound.code
                 result = error_dict("api_error", "task doesnt exist")
             else:
                 tasklist = request.dbsession.query(TaskListModel)\
                     .filter(TaskListModel.list_id == task.list_id).one_or_none()
                 if tasklist.user_id != request.user.user_id:
-                    status_code = httpexceptions.HTTPUnauthorized.status_code
+                    status_code = httpexceptions.HTTPUnauthorized.code
                     result = error_dict("api_error", "not authenticated for this request")
                 else:
                     request.dbsession.delete(task)
                     request.dbsession.flush()
-                    status_code = httpexceptions.HTTPOk.status_code
+                    status_code = httpexceptions.HTTPOk.code
                     result = "task " + str(task_id) + " deleted"
 
         return Response(
