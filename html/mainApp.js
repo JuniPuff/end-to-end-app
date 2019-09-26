f4dc43a3dfc969546652b1dafbb93ba4ff089e29
JuniPuff <juniper@todandlorna.com>
2019-09-26 14:16:48 -0600

    Remove placeholder frontend

--- html/mainApp.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('mainApp', []);
-app.controller('mainCtrl', function(){
-    var me = this;
-    me.projectPosts = projectPosts;
-    me.displayPopUpToggle = true;
-    me.displayPopUp = function(){
-        me.displayPopUpToggle = !me.displayPopUpToggle
-    }
-})
