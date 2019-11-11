5b6fccb76727707776bfb1459513f93db7ac0970
JuniPuff <juniper@todandlorna.com>
2019-11-10 17:28:27 -0700

    Pull server_user and server_ip into CI\CD secret vars

--- thing.bash
+++ thing.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ubuntu@3.18.103.85 '
+ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_IP} '
 cd /var/www/
 git pull
 '
