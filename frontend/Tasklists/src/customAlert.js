1443d4edfe49185e9ceb6ee9bac437d93be7e465
JuniPuff <juniper@todandlorna.com>
2019-11-11 14:45:13 -0700

    Remove a lot of the repeated code for buttons in customAlert

--- frontend/Tasklists/src/customAlert.js
+++ frontend/Tasklists/src/customAlert.js
@@ -3,26 +3,37 @@ import React from 'react';
 export {CustomAlert}
 
 function CustomAlert(props) {
+    function singleButton(returnValue, name){
+        return(
+            React.createElement("div", 
+                {className: returnValue ? "customButton" : "customRedButton",
+                onClick: () =>{props.handleButtons(returnValue)}},
+                name
+            )
+        )
+        
+    }
+
     function getButtons() {
         switch(props.type) {
             case "yes/no":
                 return (
                     React.createElement("div", {className: "alertButtonContainer"},
-                        React.createElement("div", {className: "customButton", onClick: () =>{props.handleButtons(true)}}, "yes"),
-                        React.createElement("div", {className: "customRedButton", onClick: () =>{props.handleButtons(false)}}, "no")
+                        singleButton(true, "yes"),
+                        singleButton(false, "no")
                     )
                 )
             case "ok/cancel":
             return (
                 React.createElement("div", {className: "alertButtonContainer"},
-                    React.createElement("div", {className: "customButton", onClick: () =>{props.handleButtons(true)}}, "ok"),
-                    React.createElement("div", {className: "customRedButton", onClick: () =>{props.handleButtons(false)}}, "cancel")
+                    singleButton(true, "ok"),
+                    singleButton(false, "cancel")
                 )
             )
             case "ok":
                 return (
                     React.createElement("div", {className: "alertButtonContainer"},
-                        React.createElement("div", {className: "customButton", onClick: () =>{props.handleButtons(true)}}, "ok")
+                        singleButton(true, "ok")
                     )
                 )
         }
