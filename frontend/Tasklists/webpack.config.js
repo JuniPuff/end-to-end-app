c316e00b750ac0500bacf778c9fdfd14a93b3d62
JuniPuff <juniper@todandlorna.com>
2019-11-12 17:30:27 -0700

    Do a dry-run of rsync

--- frontend/Tasklists/webpack.config.js
+++ frontend/Tasklists/webpack.config.js
@@ -3,12 +3,11 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        //main: './src/index.html',
         tasklists: './src/tasklists/tasklists.js'
     },
     devtool: 'eval',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist/tasklists'),
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
     },
 };
