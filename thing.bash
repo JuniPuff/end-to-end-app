484684131c4a9542ab4c925e62979d4c83223fc4
JuniPuff <juniper@mothwithalamp.com>
2019-11-13 13:56:26 -0700

    See if ssh still works just to be sure'

--- thing.bash
+++ thing.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -a --dry-run dist/ $SERVER_USER@$SERVER_IP:/var/www/frontend/Tasklists/dist
-# ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_IP} '
-# cd /var/www/
-# git pull
-# '
+# rsync -a --dry-run dist/ $SERVER_USER@$SERVER_IP:/var/www/frontend/Tasklists/dist
+ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_IP} '
+cd /var/www/
+git pull
+'
