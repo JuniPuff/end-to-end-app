ed35677db007ec023add01ca0794846f6bdba5f7
JuniPuff <juniper@todandlorna.com>
2019-11-13 14:10:15 -0700

    ssh works fine, now re-add rsync

--- thing.bash
+++ thing.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# rsync -a --dry-run dist/ $SERVER_USER@$SERVER_IP:/var/www/frontend/Tasklists/dist
 ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_IP} '
 cd /var/www/
 git pull
