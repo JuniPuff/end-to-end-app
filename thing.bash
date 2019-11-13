305462df65bada2ed7f2b7566985d64068518c5e
JuniPuff <juniper@mothwithalamp.com>
2019-11-13 14:52:30 -0700

    Fix typo on -e flag

--- thing.bash
+++ thing.bash
@@ -4,4 +4,4 @@ ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_IP} '
 cd /var/www/
 git pull
 '
-rsync -a --dry-run -r "ssh -i ${KEY_PATH}" dist/ ${SERVER_USER}@${SERVER_IP}:/var/www/frontend/Tasklists/dist
+rsync -a --dry-run -e "ssh -i ${KEY_PATH}" dist/ ${SERVER_USER}@${SERVER_IP}:/var/www/frontend/Tasklists/dist
