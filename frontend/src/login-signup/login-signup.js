a005bbd051aa6e8ac35030201aadb4ad16ce80e3
JuniPuff <juniper@mothwithalamp.com>
2019-11-18 17:41:46 -0700

    Get sign up page working

--- frontend/src/login-signup/login-signup.js
+++ frontend/src/login-signup/login-signup.js
@@ -15,8 +15,13 @@ function LoginSignup(props) {
 
     //Shared
     const [Password, setPassword] = React.useState("");
+    const [sendingRequest, setSendingRequest] = React.useState(false)
+
     const [displayError, setDisplayError] = React.useState(false);
     const [errorValue, setErrorValue] = React.useState("error: this is shown before being set")
+
+    const [displaySuccess, setDisplaySuccess] = React.useState(false);
+    const [successValue, setSuccesssValue] = React.useState("success: or is it? because this is shown before being set")
 
     function ValidateEmail(mail) {
         if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
@@ -30,11 +35,16 @@ function LoginSignup(props) {
         console.log("error: " + errorData["d"]["errors"][0])
         var error_type = errorData["d"]["error_type"]
         var error = errorData["d"]["errors"][0]
+        setErrorValue("error: " + error);
+        setDisplayError(true);
         switch (error) {
             case "user doesn't exist":
-                setErrorValue("error: user doesn't exist");
+                setErrorValue("error: username or password are incorrect");
                 setDisplayError(true);
                 break;
+            case "username already in use":
+                setErrorValue("error: username already in use");
+                setDisplayError(true);
         }
     }
 
@@ -87,27 +97,73 @@ function LoginSignup(props) {
         else {
             setDisplayError(false);
         }
-        
-        if (ValidateEmail(UsernameEmail))
-            var loginPost = postRequest("sessions", {"user_email": UsernameEmail,
-                                                        "user_pass": Password,
-                                                        "token": localStorage.getItem("token")})
-        else {
-            var loginPost = postRequest("sessions", {"user_name": UsernameEmail,
-                                                        "user_pass": Password,
-                                                        "token": localStorage.getItem("token")})
-        }
+        if (!sendingRequest) {
+            if (ValidateEmail(UsernameEmail)) {
+                var loginPost = postRequest("sessions", {"user_email": UsernameEmail,
+                                                            "user_pass": Password,
+                                                            "token": localStorage.getItem("token")})
+                setSendingRequest(true)
+            }
+            else {
+                var loginPost = postRequest("sessions", {"user_name": UsernameEmail,
+                                                            "user_pass": Password,
+                                                            "token": localStorage.getItem("token")})
+                setSendingRequest(true)
+            }
 
-        loginPost.then(function(result){
-            localStorage.setItem("token", result.d.token)
-            alert("logged in")
-        }).catch(function(errorData){
-            LoginSignupErrorHandler(errorData)
-        })
+            loginPost.then(function(result){
+                setSendingRequest(false)
+                localStorage.setItem("token", result.d.token)
+                setSuccesssValue("successfully logged in");
+                setDisplaySuccess(true);
+            }).catch(function(errorData){
+                setSendingRequest(false);
+                LoginSignupErrorHandler(errorData);
+            })
+        }
     }
 
     function handleSignupSubmit() {
-        return;
+        if (!Username) {
+            setErrorValue("error: username is required");
+            setDisplayError(true);
+            return;
+        } else if (ValidateEmail(Username)) {
+            setErrorValue("error: you put your email in the wrong field")
+            setDisplayError(true);
+            return;
+        }
+        else if (!Email || !ValidateEmail(Email)) {
+            setErrorValue("error: valid email is required");
+            setDisplayError(true);
+            return;
+        }
+        else if (!Password) {
+            setErrorValue("error: password is required")
+            setDisplayError(true);
+            return;
+        }
+        else if (PasswordAgain != Password) {
+            setErrorValue("error: passwords do not match");
+            setDisplayError(true);
+            return;
+        }
+        else {
+            setDisplayError(false);
+        }
+        if (!sendingRequest) {
+            var signupRequest = postRequest("users", {"user_name": Username, "user_email": Email, "user_pass": Password})
+            setSendingRequest(true)
+
+            signupRequest.then(function(result){
+                setSendingRequest(false)
+                setSuccessValue("successfully signed up");
+                setDisplaySuccess(true);
+            }).catch(function(errorData){
+                setSendingRequest(false);
+                LoginSignupErrorHandler(errorData);
+            });
+        }
     }
 
     if (window.location.pathname == "/login/") {
@@ -118,6 +174,7 @@ function LoginSignup(props) {
                 React.createElement('input', {className:"customInput", placeholder:"Username/Email", onChange: handleInputs, onKeyDown: checkEnter}),
                 React.createElement('input', {className:"customInput", placeholder:"password", type:"password", onChange: handleInputs, onKeyDown: checkEnter}),
                 (displayError && React.createElement('p', {className:"error"}, errorValue)),
+                (displaySuccess && React.createElement('p', {className:"success"}, successValue)),
                 React.createElement('div', {className:"inputButtonContainer"},
                     React.createElement('div', {className:"miniButton"},
                         React.createElement('button', {className:"inputButton", onClick:() => {window.location.href="/forgotpassword"}},
@@ -136,10 +193,12 @@ function LoginSignup(props) {
         return (
             React.createElement('div', {className:"centerContainer"},
                 React.createElement('h1', {className:"inputName"}, "Sign up"),
-                React.createElement('input', {className:"customInput", placeholder:"Username"}),
-                React.createElement('input', {className:"customInput", placeholder:"Email"}),
-                React.createElement('input', {className:"customInput", placeholder:"password", type:"password"}),
-                React.createElement('input', {className:"customInput", placeholder:"password again", type:"password"}),
+                React.createElement('input', {className:"customInput", placeholder:"Username", onChange: handleInputs, onKeyDown: checkEnter}),
+                React.createElement('input', {className:"customInput", placeholder:"Email", onChange: handleInputs, onKeyDown: checkEnter}),
+                React.createElement('input', {className:"customInput", placeholder:"password", type:"password", onChange: handleInputs, onKeyDown: checkEnter}),
+                React.createElement('input', {className:"customInput", placeholder:"password again", type:"password", onChange: handleInputs, onKeyDown: checkEnter}),
+                (displayError && React.createElement('p', {className:"error"}, errorValue)),
+                (displaySuccess && React.createElement('p', {className:"success"}, successValue)),
                 React.createElement('div', {className:"inputButtonContainer"},
                     React.createElement('button', {className:"inputButton", onClick:() => {window.location.href="/login"}},
                         "Login"),
