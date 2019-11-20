cd59f571cba6be93a4e6358c2075bb513c93683a
JuniPuff <juniper@todandlorna.com>
2019-11-19 17:15:31 -0700

    Move single files out of folders

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        "login-signup": './src/login-signup/login-signup.js',
-        verify: './src/verify/verify.js',
+        "login-signup": './src/login-signup.js',
+        verify: './src/verify.js',
         homePage: './src/homePage.js',
         tasklists: './src/tasklists/tasklists.js'
     },
