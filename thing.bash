0422104b746b02106d672cbec1cc830b262a1d78
JuniPuff <juniper@mothwithalamp.com>
2021-02-11 15:50:31 -0700

    Change env var name

--- thing.bash
+++ thing.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_IP} '
+ssh -i ${KEY_PATH} -oStrictHostKeyChecking=no ${SERVER_USER}@${SERVER_ADDRESS} '
 cd /var/www/
 git pull
 '
